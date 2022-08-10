8edfc77a0fb04ecbcf74a5c6689f8605f505baca
Julián Marrades <julian.marrades@hotmail.es>
2022-08-10 11:05:01 +0300

    Refactored RK4 and added progress bar

--- manimlib/physics/integrator.py
+++ manimlib/physics/integrator.py
@@ -4,6 +4,7 @@ import numpy as np
 from manimlib.constants import DIMENSIONS
 
 from manimlib.physics.physical_system import PhysicalSystem
+from manimlib.utils.simple_functions import printProgressBar
 
 
 def symplectic_euler(
@@ -30,7 +31,7 @@ def symplectic_euler(
     # For verbose stuff
     percentages: list[int] = []
     if verbose:
-        print("Integrating with Symplectic Euler. PROGRESS:", end='')
+        print("Integrating with Symplectic Euler...")
 
     n_bodies: int = system.get_n_bodies()
     n_tpoints: int = t.shape[0]
@@ -56,19 +57,13 @@ def symplectic_euler(
         positions[i] = positions[i-1] + dt * velocities[i]
         # Print stuff if verbose
         if verbose:
-            percentage: int = round(i/(n_tpoints-1)*100)
-            if percentage not in percentages and percentage%25==0:
-                percentages.append(percentage)
-                print(f" {percentage}%", end='', flush=True)
+            printProgressBar(i, n_tpoints-1, "Progress: ", "Complete", length=50)
     
     # Set the initial state back in the system
     system.set(
         positions=positions[0],
         velocities=velocities[0]
     )
-    # If verbose, break line
-    if verbose:
-        print()
     # Return
     return positions, velocities
 
@@ -96,7 +91,7 @@ def rk4(
     # For verbose stuff
     percentages: list[int] = []
     if verbose:
-        print("Integrating with 4th order Runge-Kutta. PROGRESS:", end='')
+        print("Integrating with 4th order Runge-Kutta...")
 
     n_bodies: int = system.get_n_bodies()
     n_tpoints: int = t.shape[0]
@@ -117,40 +112,37 @@ def rk4(
         )
         k1 = (velocities[i-1], system.compute_accelerations())
         # k2
+        k2_vel = velocities[i-1]+dt*k1[1]/2
         system.set(
             positions=positions[i-1]+dt*k1[0]/2,
-            velocities=velocities[i-1]+dt*k1[1]/2
+            velocities=k2_vel
         )
-        k2 = (velocities[i-1]+dt*k1[1]/2, system.compute_accelerations())
+        k2 = (k2_vel, system.compute_accelerations())
         # k3
+        k3_vel = velocities[i-1]+dt*k2[1]/2
         system.set(
             positions=positions[i-1]+dt*k2[0]/2,
-            velocities=velocities[i-1]+dt*k2[1]/2
+            velocities=k3_vel
         )
-        k3 = (velocities[i-1]+dt*k2[1]/2, system.compute_accelerations())
+        k3 = (k3_vel, system.compute_accelerations())
         # k4
+        k4_vel = velocities[i-1]+dt*k3[1]
         system.set(
             positions=positions[i-1]+dt*k3[0],
-            velocities=velocities[i-1]+dt*k3[1]
+            velocities=k4_vel
         )
-        k4 = (velocities[i-1]+dt*k3[1], system.compute_accelerations())
+        k4 = (k4_vel, system.compute_accelerations())
         # Aggregate all components and store new positions and velocities
         positions[i] = positions[i-1] + dt*(k1[0]+2*k2[0]+2*k3[0]+k4[0])/6
         velocities[i] = velocities[i-1] + dt*(k1[1]+2*k2[1]+2*k3[1]+k4[1])/6
         # Print stuff if verbose
         if verbose:
-            percentage: int = round(i/(n_tpoints-1)*100)
-            if percentage not in percentages and percentage%25==0:
-                percentages.append(percentage)
-                print(f" {percentage}%", end='', flush=True)
+            printProgressBar(i, n_tpoints-1, "Progress: ", "Complete", length=50)
     
     # Set the initial state back in the system
     system.set(
         positions=positions[0],
         velocities=velocities[0]
     )
-    # If verbose, break line
-    if verbose:
-        print()
     # Return
     return positions, velocities
